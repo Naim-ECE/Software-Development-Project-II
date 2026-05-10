3d8c97079bfb11c11c87f5b5e98dc9635cb07ffa
Md. Naimur Rahman Naim <naimur2210026@gmail.com>
2026-05-10 16:19:22 +0600

    created and run the server

--- Full-Stack-Practice/MERN-AUTH/Backend/index.js
+++ Full-Stack-Practice/MERN-AUTH/Backend/index.js
@@ -1,4 +1,4 @@
-const express = require("express");
+import express from "express";
 
 const app = express();
 
@@ -8,7 +8,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-const PORT = 5000;
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- Full-Stack-Practice/MERN-AUTH/Frontend/src/components/Header.jsx
+++ Full-Stack-Practice/MERN-AUTH/Frontend/src/components/Header.jsx
@@ -3,29 +3,31 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <div className="bg-slate-200">
-      <div className="max-w-7xl mx-auto flex items-center justify-between">
-        <Link to="/">
-          <h1 className="text-3xl font-bold text-gray-800 p-4">Auth App</h1>
-        </Link>
-        <ul className="flex space-x-4 p-4">
+    <>
+      <div className="bg-slate-200">
+        <div className="max-w-7xl mx-auto flex items-center justify-between">
           <Link to="/">
-            <li className="text-gray-600 hover:text-gray-800 cursor-pointer">
-              Home
-            </li>
+            <h1 className="text-3xl font-bold text-gray-800 p-4">Auth App</h1>
           </Link>
-          <Link to="/about">
-            <li className="text-gray-600 hover:text-gray-800 cursor-pointer">
-              About
-            </li>
-          </Link>
-          <Link to="/signin">
-            <li className="text-gray-600 hover:text-gray-800 cursor-pointer">
-              Sign In
-            </li>
-          </Link>
-        </ul>
+          <ul className="flex gap-4 p-4">
+            <Link to="/">
+              <li className="text-gray-600 hover:text-gray-800 cursor-pointer">
+                Home
+              </li>
+            </Link>
+            <Link to="/about">
+              <li className="text-gray-600 hover:text-gray-800 cursor-pointer">
+                About
+              </li>
+            </Link>
+            <Link to="/signin">
+              <li className="text-gray-600 hover:text-gray-800 cursor-pointer">
+                Sign In
+              </li>
+            </Link>
+          </ul>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
